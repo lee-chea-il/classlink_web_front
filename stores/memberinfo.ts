8684de7f94c8744dfe25ccb19d265d8ff0543ec9
akimdh <akimdh@naver.com>
2022-10-01 15:53:09 +0900

    stores 정리

--- stores/memberinfo.ts
+++ stores/memberinfo.ts
@@ -1,34 +1,93 @@
-import { defineStore } from "pinia";
-import axios from "axios";
+import { defineStore } from 'pinia';
+import axios from "axios"
 
-export const memberInfo = defineStore("memberInfo", () => {
+export const userInfo = defineStore('MemberInfo', () => {
   const myInfo = ref({
-    id: "",
-    identity: "",
+    id:'',
+    identity:''
   });
-  const setMemberInfo = (type, data) => {
+  //get
+  const setMyInfo = (type,data) => {
     myInfo.value[type] = data;
-    console.log("ssss");
-    try {
-      console.log("3333");
-      const response = axios
-        .get(`https://jsonplaceholder.typicode.com/users`)
-        //const response = await axios.get(`https://jsonplaceholder.typicode.com/users`)//'http://112.171.101.31:3106/aip/data/1'
-        .then((res) => {
-          console.log(`status: ${res.status}`); // 응답 Status code
-          console.log(`headers: ${res.headers}`); // 응답 Headers
-          console.log(`data: ${res.data}`); // 응답 Data
-        });
-      console.log("response   ", response);
-      /*const response = await axios.get('url주소',{
-        params:{
-          //url 뒤에 붙는 param id값
-          id: 12345
-        }
-      });*/
-    } catch (error) {
-      console.log("error");
+    try{
+      const response = axios.get(`http://localhost:8301/puttest`)
+      .then(res => {
+        console.log(`status: ${JSON.stringify(res.status)}`);     // 응답 Status code
+        console.log(`headers: ${JSON.stringify(res.headers)}`);   // 응답 Headers
+        console.log(`data: ${JSON.stringify(res.data)}`);         // 응답 Data
+      });
+      console.log('response   ',response);
+    }catch(error){
+      console.log('error');
     }
   };
-  return { myInfo, setMemberInfo };
-});
+  
+  //put
+  const searchId = async (email) => {
+    const bookData ={
+      "memId": "user1",
+      "memPwd":"zncbb12",
+      "memEmail":"a777@test.com",
+      "memName":"닉네임1"
+    }
+    try{
+      //insert
+      /*
+      const bookData ={
+        "memId": "user1",
+        "memPwd":"zncbb12",
+        "memEmail":"a777@test.com",
+        "memName":"닉네임1"
+      }
+      */
+      const response = axios.post(`http://localhost:8301/insertMember`,bookData)
+      .then(res => {
+        console.log(`status: ${JSON.stringify(res.status)}`);     // 응답 Status code
+        console.log(`headers: ${JSON.stringify(res.headers)}`);   // 응답 Headers
+        console.log(`data: ${JSON.stringify(res.data)}`);         // 응답 Data
+      });
+      console.log('response   ',response);
+
+      //delete
+      /*const bookData ={
+        "memIdx": 24
+      }
+      const response = axios.delete(`http://localhost:8301/deleteMember`,{data:bookData})
+      .then(res => {
+        console.log(`status: ${JSON.stringify(res.status)}`);     // 응답 Status code
+        console.log(`headers: ${JSON.stringify(res.headers)}`);   // 응답 Headers
+        console.log(`data: ${JSON.stringify(res.data)}`);         // 응답 Data
+      });
+      console.log('response   ',response);*/
+      
+      /*
+      //put
+      const bookData ={
+        "memIdx": 24,
+        "memPwd": "1111czdd"
+      }
+      const response = axios.put(`http://localhost:8301/updateMember`,bookData)
+      .then(res => {
+        console.log(`status: ${JSON.stringify(res.status)}`);     // 응답 Status code
+        console.log(`headers: ${JSON.stringify(res.headers)}`);   // 응답 Headers
+        console.log(`data: ${JSON.stringify(res.data)}`);         // 응답 Data
+      });
+      console.log('response   ',response);
+      */
+
+      /*
+      //get
+      const response = axios.get(`http://localhost:8301/puttest`)
+      .then(res => {
+        console.log(`status: ${JSON.stringify(res.status)}`);     // 응답 Status code
+        console.log(`headers: ${JSON.stringify(res.headers)}`);   // 응답 Headers
+        console.log(`data: ${JSON.stringify(res.data)}`);         // 응답 Data
+      });
+      console.log('response   ',response,email);
+      */
+
+      return response;
+    }catch(e){}
+  }
+  return { myInfo, setMyInfo, searchId }
+})
