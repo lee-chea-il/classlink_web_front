e19289f6053b8b31a9f7b830446a64fa9df92e21
akimdh <akimdh@naver.com>
2022-10-28 09:42:58 +0900

    커리큘럼 수정 중 오류 제거

--- pages/curriculum/index.vue
+++ pages/curriculum/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <PageHeader title="자료실" />
+    <PageHeader title="커리큘럼" />
     <div class="tab-content depth03 ac_manage_dtr">
 				<div class="tab-pane active">
 					<!-- 컨트롤 버튼 영역 -->
@@ -22,171 +22,49 @@
 						<!-- 왼쪽 영역 -->
 						<div class="divide_area left">
 							<!-- 탭 컨텐츠 -->
-							<ul class="nav nav-tabs" id="myTab" role="tablist">
+							<ul id="myTab" class="nav nav-tabs" role="tablist">
 								<li class="nav-item" role="presentation">
-									<button class="nav-link active" id="grade-tab" data-toggle="tab" data-target="#institute" type="button" role="tab" aria-controls="home" aria-selected="true">
+									<button id="grade-tab" class="nav-link active" data-toggle="tab" data-target="#institute" type="button" role="tab" aria-controls="home" aria-selected="true">
 										교육기관
 									</button>
 								</li>
 								<li class="nav-item" role="presentation">
-									<button class="nav-link" id="class-tab" data-toggle="tab" data-target="#franchise" type="button" role="tab" aria-controls="profile" aria-selected="false">
+									<button id="class-tab" class="nav-link" data-toggle="tab" data-target="#franchise" type="button" role="tab" aria-controls="profile" aria-selected="false">
 										프랜차이즈
 									</button>
 								</li>
 							</ul>
-							<div class="tab-content" id="myTabContent">
+							<div id="myTabContent" class="tab-content">
 								<!-- 탭 내용01 -->
-								<div class="tab-pane fade show active" id="institute" role="tabpanel" aria-labelledby="grade-tab">
-									<ul>
-										<li>
-											<!-- 리스트01 -->
-											<div class="list">
-												<div class="custom-control custom-checkbox form-inline">
-													<input type="checkbox" class="custom-control-input" id="chkA01">
-													<label class="custom-control-label" for="chkA01"></label>
-												</div>
-												<i id="show_sublist01" class="btn icons_arrow_r"></i>
-												<span class="text">국어</span>
-												<i class="icons_plus_circle_off"></i>
-											</div>
-											<!-- 리스트01_sub-->
-											<div id="list_sub01" class="list_sub" style="display: none;">
-												<ul>
-													<li>
-														<div class="custom-control custom-checkbox form-inline">
-															<input type="checkbox" class="custom-control-input" id="chkB01">
-															<label class="custom-control-label" for="chkB01"></label>
-														</div>
-														<i id="show_sublist01_sub" class="btn icons_arrow_r"></i>
-														<span class="text">대단원1</span>
-														<i class="icons_plus_circle_off"></i>
-													</li>
-												</ul>
-											</div>
-											<!-- /.리스트01_sub-->
-											<!-- 리스트01_sub_sub-->
-											<div id="list_sub01_sub" class="list_sub_sub" style="display: none;">
-												<ul>
-													<li>
-														<div class="custom-control custom-checkbox form-inline">
-															<input type="checkbox" class="custom-control-input" id="chkC01">
-															<label class="custom-control-label" for="chkC01"></label>
-														</div>
-														<span class="text">자연수의 혼합 계산 영상1.mp4</span>
-														<i class="btn icons_mu_off more_mu">
-															<div class="more_list" style="display: none;">
-																<ul>
-																	<li>다운로드</li>
-																	<li>수정</li>
-																	<li>보기</li>
-																	<li>삭제</li>
-																	<li>복사</li>
-																</ul>
-															</div>
-														</i>
-													</li>
-													<li>
-														<div class="custom-control custom-checkbox form-inline">
-															<input type="checkbox" class="custom-control-input" id="chkC02">
-															<label class="custom-control-label" for="chkC02"></label>
-														</div>
-														<span class="text">자연수의 혼합 계산 영상2.mp4</span>
-														<i class="btn icons_mu_off more_mu">
-															<div class="more_list" style="display: none;">
-																<ul>
-																	<li>다운로드</li>
-																	<li>수정</li>
-																	<li>보기</li>
-																	<li>삭제</li>
-																	<li>복사</li>
-																</ul>
-															</div>
-														</i>
-													</li>
-												</ul>
-											</div>
-											<!-- /.리스트01_sub_sub-->
-											<!-- /.리스트01 -->
-										</li>
-										<li>
-											<!-- 리스트02 -->
-											<div class="list">
-												<div class="custom-control custom-checkbox form-inline">
-													<input type="checkbox" class="custom-control-input" id="chkA02">
-													<label class="custom-control-label" for="chkA02"></label>
-												</div>
-												<i id="show_sublist02" class="btn icons_arrow_r"></i>
-												<span class="text">수학</span>
-												<i class="icons_plus_circle_off"></i>
-											</div>
-											<div id="list_sub02" class="list_sub" style="display: none;">
-												<ul>
-													<li>
-														<div class="custom-control custom-checkbox form-inline">
-															<input type="checkbox" class="custom-control-input" id="chkB02">
-															<label class="custom-control-label" for="chkB02"></label>
-														</div>
-														<i id="show_sublist02_sub" class="btn icons_arrow_r"></i>
-														<span class="text">자연수 I</span>
-														<i class="icons_plus_circle_off"></i>
-													</li>
-												</ul>
-											</div>
-											<!-- /.리스트02 -->
-										</li>
-									</ul>
+								<div id="institute" class="tab-pane fade show active" role="tabpanel" aria-labelledby="grade-tab">
+									내 커리큘럼
 								</div>
 								<!-- /.탭 내용01 -->
+
 								<!-- 탭 내용02 -->
-								<div class="tab-pane fade" id="franchise" role="tabpanel" aria-labelledby="class-tab">
-									프랜차이즈 동일 구성
+								<div id="franchise" class="tab-pane fade" role="tabpanel" aria-labelledby="class-tab">
+									내 커리큘럼
 								</div>
 								<!-- /.탭 내용02 -->
 							</div>
 							<!-- /.탭 컨텐츠 -->
 						</div>
 						<!-- /.왼쪽 영역 -->
+
 						<!-- 오른쪽 영역 -->
 						<div class="divide_area right">
 							<!-- 탭 컨텐츠 -->
-							<ul class="nav nav-tabs" id="myTab" role="tablist">
+							<ul id="myTab" class="nav nav-tabs" role="tablist">
 								<li class="nav-item" role="presentation">
-									<button class="nav-link active" id="grade-tab" data-toggle="tab" data-target="#mydata" type="button" role="tab" aria-controls="home" aria-selected="true">
+									<button id="grade-tab" class="nav-link active" data-toggle="tab" data-target="#mydata" type="button" role="tab" aria-controls="home" aria-selected="true">
 										내 커리큘럼
 									</button>
 								</li>
 							</ul>
-							<div class="tab-content" id="myTabContent">
+							<div id="myTabContent" class="tab-content">
 								<!-- 탭 내용01 -->
-								<div class="tab-pane fade show active" id="mydata" role="tabpanel" aria-labelledby="grade-tab">
-									<ul>
-										<li>
-											<!-- 리스트01 -->
-											<div class="list">
-												<div class="custom-control custom-checkbox form-inline">
-													<input type="checkbox" class="custom-control-input" id="chkAr01">
-													<label class="custom-control-label" for="chkAr01"></label>
-												</div>
-												<i id="show_sublist01" class="btn icons_arrow_r"></i>
-												<span class="text">국어</span>
-												<i class="icons_plus_circle_off"></i>
-											</div>
-											<!-- /.리스트01 -->
-										</li>
-										<li>
-											<!-- 리스트02 -->
-											<div class="list">
-												<div class="custom-control custom-checkbox form-inline">
-													<input type="checkbox" class="custom-control-input" id="chkAr02">
-													<label class="custom-control-label" for="chkAr02"></label>
-												</div>
-												<i id="show_sublist02" class="btn icons_arrow_r"></i>
-												<span class="text">수학</span>
-												<i class="icons_plus_circle_off"></i>
-											</div>
-											<!-- /.리스트02 -->
-										</li>
-									</ul>
+								<div id="mydata" class="tab-pane fade show active" role="tabpanel" aria-labelledby="grade-tab">
+									내 커리큘럼
 								</div>
 								<!-- /.탭 내용01 -->
 							</div>
@@ -204,24 +82,13 @@
 import PageHeader from '@/components/common/PageHeader.vue'
 
 export default {
-  name: 'Curriculum',
+  name: 'MyCurriculum',
   components: {
     PageHeader,
   },
   layout: 'EducationLayout',
   data() {
     return {
-      uploadType: '',
-      uploadFile: {},
-      modalDesc: {
-        open: false,
-        title: '',
-        desc: '',
-      },
-      urlData: {
-        youtube: '',
-        page: '',
-      },
     }
   },
   methods: {
