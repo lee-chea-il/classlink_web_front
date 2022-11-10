3cd7057ac9b2c2ae6f5139b1d691041af93e3f5b
yoojinkim <lastepisode@itsnowball.com>
2022-11-10 09:58:35 +0900

    221110 #front 김유진 마이홈 캘린더 적용

--- components/myhome/Calendar.vue
+++ components/myhome/Calendar.vue
@@ -1,193 +1,33 @@
 <template>
   <div class="calendar_section">
     <!-- 캘린더 영역 -->
-    <div class="calendar">
-      <div class="date_area">
-        <i class="icons_arrow_circle_l_off"></i>
-        <span class="date">12월 31일 월요일</span>
-        <i class="icons_arrow_circle_r_off"></i>
-      </div>
-      <div class="calendar_data">
-        <table class="calendar_table" cellspacing="0" cellpadding="0">
-          <thead>
-            <tr class="week_tit">
-              <th>S</th>
-              <th>M</th>
-              <th>T</th>
-              <th>W</th>
-              <th>T</th>
-              <th>F</th>
-              <th>S</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="week first">
-              <td class="calendar-day">
-                <span class="daynum">1</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">2</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">3</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">4</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">5</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">6</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">7</span>
-              </td>
-            </tr>
-            <tr class="week second">
-              <td class="calendar-day">
-                <span class="daynum">8</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">9</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">10</span>
-              </td>
-              <td class="calendar-day today">
-                <!-- 오늘 날짜 : today -->
-                <span class="daynum">11</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">12</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">13</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">14</span>
-              </td>
-            </tr>
-            <tr class="week third">
-              <td class="calendar-day">
-                <span class="daynum">15</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">16</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">17</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">18</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">19</span>
-              </td>
-              <td class="calendar-day active">
-                <!-- 선택한 날짜 : active -->
-                <span class="daynum">20</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">21</span>
-              </td>
-            </tr>
-            <tr class="week fourth">
-              <td class="calendar-day">
-                <span class="daynum">22</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">23</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">24</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">25</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">26</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">27</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">28</span>
-              </td>
-            </tr>
-            <tr class="week last">
-              <td class="calendar-day">
-                <span class="daynum">29</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">30</span>
-              </td>
-              <td class="calendar-day">
-                <span class="daynum">31</span>
-              </td>
-              <td class="calendar-day calendar-other-month">
-                <span class="daynum">1</span>
-              </td>
-              <td class="calendar-day calendar-other-month">
-                <span class="daynum">2</span>
-              </td>
-              <td class="calendar-day calendar-other-month">
-                <span class="daynum">3</span>
-              </td>
-              <td class="calendar-day calendar-other-month">
-                <span class="daynum">4</span>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-    <!-- /.캘린더 영역-->
+    <DatePicker
+      v-model="date"
+      trim-weeks
+      class="custom-calendar"
+      :masks="{ title: 'YYYY MMM' }"
+      @dayclick="$emit('click-date', $event)"
+    />
+    <!-- / 캘린더 영역 -->
     <!-- 선택일자 스케쥴 -->
     <div class="schedule">
-      <div class="date">2022년 01월 01일 월</div>
+      <div class="date">{{ selectedDateTitle }}</div>
       <div class="list_area">
-        <!-- [개발참조] : 등록된 것이 없을 때 
-													<div class="nothing_txt">
-														오늘의 일정이 없습니다.
-													</div> -->
+        <div v-if="selectedDateTaskList.length === 0" class="nothing_txt">
+          오늘의 일정이 없습니다.
+        </div>
         <ul>
-          <li>
-            <div class="time">2:00</div>
+          <li v-for="(item, idx) in selectedDateTaskList" :key="idx">
+            <div class="time">{{ item.time_start }}</div>
             <div class="lesson_info">
               <div class="subject">
-                영어심화 리스닝 글이길어지면글이길어지면글이길어지면글이길어지면
+                {{ item.subject }}
               </div>
               <div class="lesson">
-                <span>영어</span><span class="divid"></span>
-                <span>심화 A반</span><span class="divid"></span>
-                <span class="teacher">홍길동 선생님</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="time">2:00</div>
-            <div class="lesson_info">
-              <div class="subject">
-                영어심화 리스닝 글이길어지면글이길어지면글이길어지면글이길어지면
-              </div>
-              <div class="lesson">
-                <span>영어</span><span class="divid"></span>
-                <span>심화 A반</span><span class="divid"></span>
-                <span class="teacher">홍길동 선생님</span>
-              </div>
-            </div>
-          </li>
-          <li>
-            <div class="time">2:00</div>
-            <div class="lesson_info">
-              <div class="subject">
-                영어심화 리스닝 글이길어지면글이길어지면글이길어지면글이길어지면
-              </div>
-              <div class="lesson">
-                <span>영어</span><span class="divid"></span>
-                <span>심화 A반</span><span class="divid"></span>
-                <span class="teacher">홍길동 선생님</span>
+                <span>{{ item.lessonTitle }}</span
+                ><span class="divid"></span> <span>{{ item.lessonClass }}</span
+                ><span class="divid"></span>
+                <span class="teacher">{{ item.teacher }}</span>
               </div>
             </div>
           </li>
@@ -198,8 +38,132 @@
   </div>
 </template>
 <script>
+import DatePicker from 'v-calendar/lib/components/date-picker.umd'
+import { computed } from 'vue'
 export default {
   name: 'Calendar',
+  components: {
+    DatePicker,
+  },
+  date() {
+    return {}
+  },
+  props: {
+    selectedDateTaskList: {
+      type: Array,
+      default: () => [],
+    },
+    selectedDate: {
+      type: Date,
+      default: null,
+    },
+    selectedDateTitle: {
+      type: String,
+      default: '',
+    },
+  },
+  setup(props, { emit }) {
+    const date = computed({
+      get: () => props.selectedDate,
+      set: (value) => emit('update:selectedDate', value),
+    })
+
+    return { date }
+  },
+  methods: {},
 }
 </script>
-<style scoped></style>
+<style scoped>
+.nothing_txt {
+  justify-content: flex-start;
+  top: 35px;
+}
+</style>
+<style lang="postcss" scoped>
+::-webkit-scrollbar {
+  width: 0px;
+}
+::-webkit-scrollbar-track {
+  display: none;
+}
+/deep/ .custom-calendar.vc-container {
+  --day-border: 1px solid #f9f9fc;
+  --day-width: 90px;
+  --day-height: 150px;
+  --weekday-bg: #f8fafc;
+  --weekday-border: 1px solid #f9f9fc;
+  --blue-600: #7391fa;
+  border: 0;
+  background-color: #f9f9fc;
+  width: 100%;
+}
+/deep/ .vc-weeks {
+  padding-top: 20px;
+  color: #2c51d7;
+  height: 250px;
+}
+/deep/ .vc-container [role='button'] {
+  cursor: pointer;
+  color: #2c51d7;
+}
+
+/deep/ .vc-weeks span {
+  font-size: 15px;
+}
+
+/deep/ .vc-weekday {
+  color: #2c51d7;
+  border-bottom: 1px solid #2c51d7;
+  padding-top: 8px;
+  padding-bottom: 4px;
+}
+
+/deep/ .vc-pane-container {
+  color: #2c51d7;
+}
+
+/deep/ .vc-svg-icon {
+  color: #7391fa;
+  background: white;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  box-shadow: 1px 1px 4px 4px #eae9e9;
+}
+
+/deep/ .vc-nav-popover-container {
+  background: #f9f9fc;
+}
+/deep/ .vc-title {
+  color: #2c51d7;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: 'Noto Sans KR', sans-serif;
+}
+/deep/ .vc-nav-item:hover {
+  background: #f9f9fc;
+}
+/deep/ .vc-nav-arrow:hover {
+  background: #f9f9fc;
+}
+/deep/ .vc-nav-title.vc-grid-focus {
+  background: #f9f9fc;
+}
+/deep/ .vc-popover-content.direction-bottom {
+  border: 1px solid #dbdbdb;
+}
+/deep/ .vc-nav-item.is-active {
+  background: #f9f9fc;
+}
+/deep/ .vc-day-content:focus {
+  background-color: rgb(204 214 224 / 0%);
+}
+/deep/ .vc-day-content:hover {
+  background-color: rgb(204 214 224 / 0%);
+}
+/deep/ .vc-arrow:hover {
+  background-color: rgb(204 214 224 / 0%);
+}
+
+.vc-day-content: hover;
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -40,7 +40,12 @@
               <div class="right_right">
                 <div class="row">
                   <div class="right_right_all">
-                    <Calendar />
+                    <Calendar
+                      :selectedDateTaskList="selectedDateTaskList"
+                      :selectedDate="selectedDate"
+                      :selectedDateTitle="selectedDateTitle"
+                      @click-date="onDayClick"
+                    />
                   </div>
                 </div>
               </div>
@@ -80,58 +85,72 @@ export default {
         {
           academy: '일산어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '영어심화리딩',
           lessonTitle: '영어',
           lessonClass: '심화 A반',
           teacher: '홍길동 선생님',
+          date: '2022-11-09',
         },
         {
           academy: '김포어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '수학심화리딩',
           lessonTitle: '수학',
           lessonClass: '심화 B반',
           teacher: '임꺽정 선생님',
+          date: '2022-11-09',
         },
         {
           academy: '광주어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '과학심화리딩',
           lessonTitle: '과학',
           lessonClass: '심화 C반',
           teacher: '임창정 선생님',
+          date: '2022-11-09',
         },
         {
           academy: '한남어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '체육심화리딩',
           lessonTitle: '체육',
           lessonClass: '심화 D반',
           teacher: '전현무 선생님',
+          date: '2022-11-10',
         },
         {
           academy: '마포어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '음악심화리딩',
           lessonTitle: '음악',
           lessonClass: '심화 E반',
           teacher: '김종국 선생님',
+          date: '2022-11-10',
         },
         {
           academy: '홍대어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '사회심화리딩',
           lessonTitle: '사회',
           lessonClass: '심화 F반',
           teacher: '유재석 선생님',
+          date: '2022-11-10',
         },
         {
           academy: '잠실어학원',
           time: '09:00 ~ 12:00',
+          time_start: '09:00',
           subject: '국어심화리딩',
           lessonTitle: '국어',
           lessonClass: '심화 G반',
           teacher: '사투리 선생님',
+          date: '2022-11-10',
         },
       ],
 
@@ -235,7 +254,39 @@ export default {
         { category: 3, subject: '반 공지입니다.', date: '2022.11.08' },
       ],
       selectedNoticeList: [],
+
+      // 캘린더
+      selectedDate: new Date(),
+      selectedDateTitle: '',
+
+      selectedDateTaskList: [],
     }
+  },
+  created() {
+    // 캘린더 날짜 변환
+    const days = ['일', '월', '화', '수', '목', '금', '토']
+    this.selectedDateTitle =
+      this.selectedDate.getFullYear() +
+      '년 ' +
+      (this.selectedDate.getMonth() + 1) +
+      '월 ' +
+      this.selectedDate.getDate() +
+      '일 ' +
+      days[this.selectedDate.getDay()] +
+      '요일'
+    const today = new Date()
+
+    const year = today.getFullYear()
+    const month = ('0' + (today.getMonth() + 1)).slice(-2)
+    const day = ('0' + today.getDate()).slice(-2)
+    const dateString = year + '-' + month + '-' + day
+    const array = []
+    for (let i = 0; i < this.todayLectureList.length; i++) {
+      if (this.todayLectureList[i].date === dateString) {
+        array.push(this.todayLectureList[i])
+      }
+    }
+    this.selectedDateTaskList = array
   },
   methods: {
     // 학습함
@@ -259,6 +310,19 @@ export default {
         }
       }
       this.selectedNoticeList = array
+    },
+    // 캘린더
+    onDayClick(day) {
+      console.log(day)
+      this.selectedDate = new Date(day.id)
+      this.selectedDateTitle = day.ariaLabel
+      const array = []
+      for (let i = 0; i < this.todayLectureList.length; i++) {
+        if (this.todayLectureList[i].date === day.id) {
+          array.push(this.todayLectureList[i])
+        }
+      }
+      this.selectedDateTaskList = array
     },
   },
 }
