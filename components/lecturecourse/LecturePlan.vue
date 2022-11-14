fcc810291d66942eab0ad57c27366903214abfa4
yoojinkim <lastepisode@itsnowball.com>
2022-11-14 09:54:16 +0900

    221110 #front 김유진 강의코스 > 강의계획서 목록, 과제함 목록 화면작업

---
+++ components/lecturecourse/LecturePlan.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="tab-content depth03 ac_manage_notice ac_manage_classplan">
+    <div class="tab-pane active">
+      <!-- 검색 영역 -->
+      <div class="search_section">
+        <div class="left_area">
+          <span class="course_tit">{{ lectureCourse.subject }}</span>
+          <span class="course_con"
+            >{{ lectureCourse.lessonTitle }} / {{ lectureCourse.lessonClass }} /
+            {{ lectureCourse.teacher }}</span
+          >
+        </div>
+        <div class="right_area">
+          <nuxt-link to="/class/lecturecourse" class="btn btn_crud_default"
+            >강의목록 보기</nuxt-link
+          >
+          <button class="btn btn_crud_default" @click="$emit('delete-plan')">
+            삭제
+          </button>
+          <div class="input-group input-search form-inline form-search">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="검색어 입력"
+              @input="$emit('change-input', $event)"
+              @keyup.enter="$emit('search-plan')"
+            />
+            <div class="input-group-append">
+              <button
+                class="btn icons_search_off"
+                type="button"
+                @click="$emit('search-plan')"
+              ></button>
+            </div>
+          </div>
+          <a href="#" class="btn btn_crud_point">등록</a>
+        </div>
+      </div>
+      <!-- /.검색 영역 -->
+      <!-- 테이블 영역 -->
+      <!-- [개발참조] 데이터 없을 경우  -->
+      <div v-if="lecturePlanList.length === 0" class="page_nodata">
+        등록된 강의계획서가 없습니다.
+      </div>
+
+      <div v-else class="table_section">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>
+                <div class="custom-control custom-checkbox form-inline">
+                  <input
+                    id="chkAll"
+                    type="checkbox"
+                    class="custom-control-input"
+                    :checked="allCheckBoxFlag"
+                    @input="$emit('checked-all', $event)"
+                  />
+                  <label class="custom-control-label" for="chkAll"></label>
+                </div>
+              </th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
+              <th>공개여부</th>
+              <th>읽은 수/전체</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody v-if="searchFlag === 0">
+            <tr v-for="(item, idx) in lecturePlanList" :key="idx">
+              <td>
+                <div class="custom-control custom-checkbox form-inline">
+                  <input
+                    :id="item.id"
+                    name="chk"
+                    type="checkbox"
+                    class="custom-control-input"
+                    @input="$emit('selete-plan', $event)"
+                  />
+                  <label class="custom-control-label" :for="item.id"></label>
+                </div>
+              </td>
+              <td>
+                <div
+                  class="classplan_tit"
+                  data-toggle="modal"
+                  data-target="#modalNoticeView"
+                  data-dismiss="modal"
+                >
+                  {{ item.title }}
+                </div>
+              </td>
+              <td>{{ item.writer }}</td>
+              <td>{{ item.created_at }}</td>
+              <td>{{ item.open ? '공개' : '비공개' }}</td>
+              <td>{{ item.views }}/{{ lectureCourse.students }}</td>
+              <td></td>
+            </tr>
+          </tbody>
+          <tbody v-if="searchFlag === 1">
+            <tr v-for="(item, idx) in searchList" :key="idx">
+              <td>
+                <div class="custom-control custom-checkbox form-inline">
+                  <input
+                    :id="item.id"
+                    name="chk-plan"
+                    type="checkbox"
+                    class="custom-control-input"
+                    @input="$emit('selete-plan', $event)"
+                  />
+                  <label class="custom-control-label" :for="item.id"></label>
+                </div>
+              </td>
+              <td>
+                <div
+                  class="classplan_tit"
+                  data-toggle="modal"
+                  data-target="#modalNoticeView"
+                  data-dismiss="modal"
+                >
+                  {{ item.title }}
+                </div>
+              </td>
+              <td>{{ item.writer }}</td>
+              <td>{{ item.created_at }}</td>
+              <td>{{ item.open ? '공개' : '비공개' }}</td>
+              <td>{{ item.views }}/{{ lectureCourse.students }}</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.테이블 영역 -->
+      <!-- 페이징 영역 -->
+      <div class="pagination_section">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item">
+              <a class="page-link" href="#">
+                <span class="previous"></span>
+              </a>
+            </li>
+            <li class="page-item">
+              <a class="page-link active" href="#">1</a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+              <a class="page-link" href="#">
+                <span class="next"></span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <!-- /.페이징 영역 -->
+      <!-- [개발참조] 데이터 없을 경우 출력됨
+					<div class="btn_sec">
+						<button class="btn btn_crud_point">등록</button>
+						<button class="btn btn_crud_default">취소</button>
+					</div> -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LecturePlan',
+  props: {
+    lecturePlanList: {
+      type: Array,
+      default: () => [],
+    },
+    lectureCourse: {
+      type: Object,
+      default: () => {},
+    },
+    searchFlag: {
+      type: Number,
+      default: 0,
+    },
+    searchList: {
+      type: Array,
+      default: () => [],
+    },
+    allCheckBoxFlag: {
+      type: Boolean,
+      dafault: false,
+    },
+  },
+  methods: {
+    onClickCheckBox({ target: { id } }) {
+      console.log(id)
+    },
+  },
+}
+</script>
+<style scoped>
+.custom-control-label::after {
+  left: -1.65rem !important;
+}
+</style>
