84f22ab2a04a4f9bac68bdc61595cca0c728394b
KimDanwoo <84695769+KimDanwoo@users.noreply.github.com>
2022-12-20 10:24:03 +0900

    2022.12.20 #front 김단우 api명 변경 -1

--- services/index.js
+++ services/index.js
@@ -1,14 +1,7 @@
 import apiLogin from './apiLogin'
 import apiMypage from './apiMypage'
-<<<<<<< Updated upstream
-import apiReference from './apiReference'
 import apiLectureCourse from './apiLectureCourse'
-import api from './api'
-
-export { apiLogin, apiMypage, apiReference, apiLectureCourse, api }
-=======
 import apiData from './apiData'
 import api from './api'
 
-export { apiLogin, apiMypage, apiData, api }
->>>>>>> Stashed changes
+export { apiLogin, apiMypage, apiLectureCourse, apiData, api }
